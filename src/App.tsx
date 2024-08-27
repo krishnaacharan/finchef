30e40a1770cc3dec9773878ac2a7cca5fae2577b
krishnaacharan <krishnacharanmrp123@gmail.com>
2024-08-27 15:12:02 +0530

    routing changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ServicesPage from "./pages/ServicesPage";
-import ResourcesPage from "./pages/ResourcesPage";
-import CaseStudiesPage from "./pages/CaseStudiesPage";
-import BlogPage from "./pages/BlogPage";
-import ContactPage from "./pages/ContactPage";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/HomePage";
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/resources" element={<ResourcesPage />} />
-        <Route path="/case-studies" element={<CaseStudiesPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
+      <HomePage />
       <Footer />
-    </Router>
+    </>
   );
 };
 
